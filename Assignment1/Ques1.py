86a14111b106f2db765b8e9b8730b71bf3246cc5
Saksham <20D170037@iitb.ac.in>
2021-08-16 12:06:25 +0530

    Q1 and Q2

---
+++ Assignment1/Ques1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+k = 10
+# change value of k here
+
+data = np.loadtxt("hw1a.txt")
+avg1 = np.array([])
+avg2 = np.array([])
+avg3 = np.array([])
+for i in range(0,50):
+
+    opt1 = data[0:k]
+    avg1 = np.append(avg1, np.mean(opt1))
+
+    rand = int(np.random.random() * 10000)
+    opt2 = data[rand:(rand+k)]
+    avg2 = np.append(avg2, np.mean(opt2))
+
+    opt3 = np.random.choice(data,k,replace=False)
+    avg3 = np.append(avg3, np.mean(opt3))
+
+optfinal = np.random.choice(data,k,replace=False)
+print("Assuming Case 3 is best:-\n" + "average: " + str(np.mean(optfinal)) + "\nstddev: " + str(np.std(optfinal)))
+
+fig, ax = plt.subplots(1,3)
+index = range(1,51)
+ax[0].scatter(index, avg1)
+ax[1].scatter(index, avg2)
+ax[2].scatter(index, avg3)
+plt.subplots_adjust(left=0.1, right=0.9, top=0.9, wspace=0.4, hspace=0.4)
+plt.show()
---
+++ Assignment1/Ques2.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def find_prob(fname):
+    with open(fname) as f:
+        data = f.readlines()
+    prob = np.array([])
+    val = np.array([])
+    for i in range(0, len(data)):
+        val = 0 if data[i]=='false\n' else 1
+        if(i==0):
+            prob = np.append(prob, val)
+        else:
+            prob = np.append(prob, (prob[i-1]*i + val)/(i+1))
+    for i in range(n-1, len(prob)):
+        if (prob[i] < pH-sr):
+            print("SURE: " + str(i+1) + " with probability " + str(prob[i]))
+        elif (prob[i] < pH-dt):
+            print("Doubt: " + str(i+1) + " with probability " + str(prob[i]))
+
+
+# initial assumption
+pH = 0.5
+
+# initial gap of n elements
+n = 10
+
+#buffer for doubt
+dt = 0.1
+
+#buffer for sure
+sr = 0.25
+
+print(find_prob("hw1b1.txt"))
+print("\n\n")
+print(find_prob("hw1b2.txt"))
+print("\n\n")
+print(find_prob("hw1b3.txt"))
+print("\n\n")
+print(find_prob("hw1b4.txt"))
